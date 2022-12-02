7966162c4f7d1fba6b43c50fca0d453a0a5f0596
Ak9500 <116661624+Ak9500@users.noreply.github.com>
2022-12-02 15:16:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var resume = [{
+    "name":"manikandan",
+    "address":"none",
+    "email":"manikandanpm880@gmail.com",
+},
+
+{
+    "batch":"2019",
+    "experience":"2 year",
+    "skils":"full stack developer"
+}]
+console.log(resume) 
